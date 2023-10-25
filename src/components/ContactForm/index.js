5b3ae46ecdea2cd89891244395ef10c72bc86586
Alexandra <alexandervirtuous14@gmail.com>
2023-10-25 11:52:59 +0100

    modified:   src/components/ContactForm/index.css 	modified:   src/components/ContactForm/index.js

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -22,53 +22,55 @@ function ContactForm() {
   return (
     <div>
       <form onSubmit={handleSubmit} className="contact-form">
-        <div className="input-group">
-          <div className="input-field">
-            <label htmlFor="name">Name:</label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              value={formData.name}
-              onChange={handleInputChange}
-              required
-            />
+        <div className="form">
+          <div className="input-group">
+            <div className="input-field">
+              <label htmlFor="name">Name:</label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                value={formData.name}
+                onChange={handleInputChange}
+                required
+              />
+            </div>
+
+            <div className="input-field">
+              <label htmlFor="subject">Subject:</label>
+              <input
+                type="text"
+                id="subject"
+                name="subject"
+                value={formData.subject}
+                onChange={handleInputChange}
+                required
+              />
+            </div>
           </div>
 
-          <div className="input-field">
-            <label htmlFor="subject">Subject:</label>
-            <input
-              type="text"
-              id="subject"
-              name="subject"
-              value={formData.subject}
-              onChange={handleInputChange}
-              required
-            />
-          </div>
-        </div>
-
-        <div className="input-group1">
-          <div className="input-field">
-            <label htmlFor="message">Message:</label>
-            <textarea
-              id="message"
-              name="message"
-              value={formData.message}
-              onChange={handleInputChange}
-              required
-            />
-          </div>
-          <div className="input-field">
-            <label htmlFor="email">Email:</label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={formData.email}
-              onChange={handleInputChange}
-              required
-            />
+          <div className="input-group1">
+            <div className="input-field">
+              <label htmlFor="email">Email:</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleInputChange}
+                required
+              />
+            </div>
+            <div className="input-field">
+              <label htmlFor="message">Message:</label>
+              <textarea
+                id="message"
+                name="message"
+                value={formData.message}
+                onChange={handleInputChange}
+                required
+              />
+            </div>
           </div>
         </div>
 
