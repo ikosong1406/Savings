245a425834bd5e08214f1ffab99eb288ef5c3e6f
Alexandra <alexandervirtuous14@gmail.com>
2023-10-29 12:24:36 +0100

    modified:   src/App.css 	modified:   src/App.js 	deleted:    src/Imgs/apple.png 	deleted:    src/Imgs/background.jpg 	deleted:    src/Imgs/bank.png 	deleted:    src/Imgs/bank.svg 	deleted:    src/Imgs/banner-mobile1.png 	deleted:    src/Imgs/chip.png 	deleted:    src/Imgs/google.png 	deleted:    src/Imgs/groupicon.jpg 	deleted:    src/Imgs/headphones.jpg 	deleted:    src/Imgs/logo.png 	deleted:    src/Imgs/logo1.png 	deleted:    src/Imgs/logo2.png 	deleted:    src/Imgs/master-card.png 	deleted:    src/Imgs/mobile-view1.png 	deleted:    src/Imgs/mobile-view2.png 	deleted:    src/Imgs/mobile-view3.png 	deleted:    src/Imgs/mobile-view4.png 	deleted:    src/Imgs/mobile-view5.png 	deleted:    src/Imgs/mobile-view6.png 	deleted:    src/Imgs/mobile-view7.png 	deleted:    src/Imgs/mobile-view8.png 	deleted:    src/Imgs/mobile.jpg 	deleted:    src/Imgs/payment.svg 	deleted:    src/Imgs/paypal.png 	deleted:    src/Imgs/personal.svg 	deleted:    src/Imgs/profile-picc.png 	deleted:    src/Imgs/sample_user.jpg 	deleted:    src/Imgs/team1.jpg 	deleted:    src/Imgs/team2.png 	deleted:    src/Imgs/team3.jpg 	deleted:    src/Imgs/transaction.svg 	deleted:    src/Imgs/visaa.png 	deleted:    src/Imgs/wallet.png 	deleted:    src/components/ContactForm/index.css 	deleted:    src/components/ContactForm/index.js 	deleted:    src/components/Navbar/index.css 	deleted:    src/components/Navbar/index.js 	deleted:    src/components/Swiper/index.css 	deleted:    src/components/Swiper/index.js 	deleted:    src/pages/Eula/index.css 	deleted:    src/pages/Eula/index.js 	deleted:    src/pages/Faq/index.css 	deleted:    src/pages/Faq/index.js 	deleted:    src/pages/Home/index.css 	deleted:    src/pages/Home/index.js 	deleted:    src/pages/Layout/index.css 	deleted:    src/pages/Layout/index.js 	deleted:    src/pages/Overview/index.css 	deleted:    src/pages/Overview/index.js 	deleted:    src/pages/Partnership/index.css 	deleted:    src/pages/Partnership/index.js 	deleted:    src/pages/Privacy/index.css 	deleted:    src/pages/Privacy/index.js 	deleted:    src/pages/Savings/index.css 	deleted:    src/pages/Savings/index.js 	deleted:    src/pages/Terms/index.css 	deleted:    src/pages/Terms/index.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Import Router
-import Home from "../src/pages/Home";
-import Overview from "../src/pages/Overview";
-import Savings from "../src/pages/Savings";
-import Layout from "../src/pages/Layout";
-import Privacy from "../src/pages/Privacy";
-import Terms from "../src/pages/Terms";
-import Eula from "../src/pages/Eula";
-import Partnership from "../src/pages/Partnership";
-import FAQ from "../src/pages/Faq";
 import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="overview" element={<Overview />} />
-          <Route path="savings" element={<Savings />} />
-          <Route path="privacy" element={<Privacy />} />
-          <Route path="terms" element={<Terms />} />
-          <Route path="eula" element={<Eula />} />
-          <Route path="partnership" element={<Partnership />} />
-          <Route path="faq" element={<FAQ />} />
-        </Route>
-      </Routes>
-    </Router>
+    <div>
+      <h1> ERROR 404 </h1>
+    </div>
   );
-}
+};
 
 export default App;
